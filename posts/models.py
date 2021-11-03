8bdd0de7902b8fd2e4e9da93d7f0e8403ba992fd
unknown <julianodesenv@gmail.com>
2021-11-03 12:28:36 -0300

    resize image upload

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from categories.models import Categories
 from django.contrib.auth.models import User
 from django.utils import timezone
+from PIL import Image
+from django.conf import settings
+import os
 
 
 class Post(models.Model):
@@ -11,8 +14,36 @@ class Post(models.Model):
     description = models.TextField()
     resume = models.TextField()
     category = models.ForeignKey(Categories, on_delete=models.DO_NOTHING, blank=True, null=True)
-    image = models.ImageField(upload_to='post_img/%Y/%m/%d', blank=True, null=True)
+    image = models.ImageField(upload_to='post_img/%Y/%m/%d', blank=True, null=True, verbose_name='Imagem')    
     publish = models.BooleanField(default=False)
     
+
     def __str__(self):
         return self.name
+
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.image:
+            self.resize_image(self.image.name, 800)
+
+
+    @staticmethod
+    def resize_image(img_name, new_width):
+        img_path = os.path.join(settings.MEDIA_ROOT, img_name)
+        img = Image.open(img_path)
+        width, height = img.size
+        new_height = round((new_width * height) / width)
+
+        if width <= new_width:
+            img.close()
+            return
+
+        new_img = img.resize((new_width, new_height), Image.ANTIALIAS)
+        new_img.save(
+            img_path,
+            optimize=True,
+            quality=60
+        )
+        new_img.close()
